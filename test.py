70a8543f3a9284004ce5aa8507146054c0ccb07d
princess80 <35203658+princess80@users.noreply.github.com>
2018-01-17 02:58:19 +0200

    first project

---
+++ test.py
@@ -0,0 +1,12 @@
+def func():
+  username=input()
+  password=int(input())
+  print username
+  print password
+
+  print "write some words you and your love that  you both only know the meaning"
+  print ('click')
+  user=input()  #btn(here)
+  print user
+
+func()
